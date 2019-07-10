After fixing rotation bugs it will be almost done

src/sources.py
@@ -33,13 +33,53 @@ class fobject:
                 if int(self.bt[i][j]) >= 1:
                     game.show_map[self.pos[1]+i][self.pos[0]+j] = float(self.bt[i][j])
 
+    def move_control(self, game, key):
+
+        if key == ord("w"):
+            self.rotate()
+
+        elif key == ord("s"):
+            self.pos[1] += 1
+
+        elif key == ord("a"):
+
+            for i in range(len(self.bt)):
+                for j in range(len(self.bt[0])):
+                    if int(self.bt[i][j]) >= 1:
+                        try:
+                            if game.game_map[self.pos[1] + i][self.pos[0] + j - 1]:
+                                return
+                        except IndexError:
+                            continue
+
+            if self.pos[0] > 0:
+                self.pos[0] += -1
+
+        elif key == ord("d"):
+            for i in range(len(self.bt)):
+                for j in range(len(self.bt[0])):
+                    j = len(self.bt[0]) - j - 1
+                    if int(self.bt[i][j]) >= 1:
+                        try:
+                            if game.game_map[self.pos[1] + i][self.pos[0] + j + 1]:
+                                return
+                        except IndexError:
+                            continue
+
+            if self.pos[0] < len(game.game_map[0])-len(self.bt[0]):
+                self.pos[0] += 1
+
+        else:
+            pass
 
 class game:
     def __init__(self, game_size=[0, 0], block_size = 1):
         self.game_size  = game_size
+
         self.game_map = np.zeros((self.game_size[0], self.game_size[1])).tolist()
         self.game_map[-1] = np.ones((self.game_size[1])).tolist()
-        self.show_map = []
+
+        self.show_map = deepcopy(self.game_map)
         self.block_size = block_size
         self.img = np.zeros((self.game_size[0]*self.block_size,
                     self.game_size[1]*self.block_size, 3), np.uint8)
@@ -53,4 +93,4 @@ class game:
 
     def draw_block(self, pos, opt=1, color = 255):
         bs = self.block_size
-        self.img[pos[0]*bs:pos[0]*bs+bs, pos[1]*bs:pos[1]*bs + bs] = color
+        self.img[pos[0]*bs+1:pos[0]*bs+bs-1, pos[1]*bs+1:pos[1]*bs + bs-1] = color